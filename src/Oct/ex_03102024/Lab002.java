d85f433ed5af3fae7774d19192019a79f7f62a7f
Ankita <mukherjee.ankita89@gmail.com>
2024-10-06 11:06:36 +0530

    Code added to explain Comments

--- src/Oct/ex_03102024/Lab002.java
+++ src/Oct/ex_03102024/Lab002.java
@@ -1,7 +1,11 @@
 package Oct.ex_03102024;
-
+/*
+* This is
+* multi-line comment,used for documentation,reference
+* Author - Ankita
+*/
 public class Lab002 {
     public static void main(String[] args) {
-
+//    System.out.println("Hello World!"); --This is a single line comment
     }
 }
