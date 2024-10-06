aa49319e2b3b5480d7ad85a6fe90399647ad42fd
Ankita <mukherjee.ankita89@gmail.com>
2024-10-06 10:43:22 +0530

    Check commit second time

---
+++ src/Task/task_03102024/Task003.java
@@ -0,0 +1,11 @@
+package Task.task_03102024;
+
+public class Task003 {
+    public static void main(String[] args) {
+        int n1=20,n2=10;
+        System.out.println("Result of addition: " +n1+ " + " +n2 +" = "+ (n1+n2));
+        System.out.println("Result of subtraction: " +n1 + " - " + n2+ " = " + (n1-n2));
+        System.out.println("Result of multiplication: " +n1 + " * " + n2+ " = " + (n1*n2));
+        System.out.println("Result of division: " +n1 + " / " + n2+ " = " + (n1/n2));
+    }
+}
