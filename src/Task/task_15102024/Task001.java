72f7a6f9ea8bf0a2a37a90e3eacc2f5b07805cdc
Ankita <mukherjee.ankita89@gmail.com>
2024-10-15 22:06:30 +0530

    Completed task1 on 15th Oct

---
+++ src/Task/task_15102024/Task001.java
@@ -0,0 +1,10 @@
+package Task.task_15102024;
+
+public class Task001 {
+    public static void main(String[] args) {
+        //Finding the largest of three numbers using ternary operator
+        int a=100,b=29,c=66;
+        int maxNum = a>b?(a>c?a:c):(b>c?b:c);
+        System.out.printf("The largest number is %d",maxNum);
+    }
+}
